157ecf098b6f75fd028f114d52c04f4c542da25c
lukester1010 <s5303770@bournemouth.ac.uk>
2022-05-15 18:14:32 +0100

    Initial Commit

--- app/src/main/java/com/example/lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/example/lifestyleapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lifestyleapp
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +10,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.example.lifestyleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        val button: Button = findViewById(R.id.WorkoutsButton)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, WorkOutPage::class.java)
+            startActivity(intent)
+        }
+    }
 }
