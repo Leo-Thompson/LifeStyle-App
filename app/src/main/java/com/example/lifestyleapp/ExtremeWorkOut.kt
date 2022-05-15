a865ae798422f5c62f49f6af31b32aab7d3589ba
lukester1010 <s5303770@bournemouth.ac.uk>
2022-05-15 18:04:21 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/lifestyleapp/ExtremeWorkOut.kt
@@ -0,0 +1,11 @@
+package com.example.lifestyleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExtremeWorkOut : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extreme_wor_out)
+    }
+}
--- app/src/main/java/com/example/lifestyleapp/WorkOutPage.kt
+++ app/src/main/java/com/example/lifestyleapp/WorkOutPage.kt
@@ -1,11 +1,30 @@
 package com.example.lifestyleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WorkOutPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_out_page)
+        title = "KotlinApp"
+        val button: Button = findViewById(R.id.easyButton)
+        button.setOnClickListener {
+            val intent = Intent(this@WorkOutPage, EasyWorkOut::class.java)
+            startActivity(intent)
+        }
+        val button2: Button = findViewById(R.id.interbtn)
+        button2.setOnClickListener{
+            val intent = Intent(this@WorkOutPage, InterWorkOut::class.java)
+            startActivity(intent)
+        }
+        val button3: Button = findViewById(R.id.extbtn)
+        button3.setOnClickListener{
+            val intent = Intent(this@WorkOutPage, ExtremeWorkOut::class.java)
+            startActivity(intent)
+        }
     }
+
 }
