d1277c5a9066b75a714a2adbec05d630e2a7cf95
lukester1010 <s5303770@bournemouth.ac.uk>
2022-05-17 19:44:44 +0100

    Total Litres of water consumed works

--- app/src/main/java/com/example/lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/example/lifestyleapp/MainActivity.kt
@@ -11,24 +11,46 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.example.lifestyleapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var total = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "KotlinApp"
         val button: Button = findViewById(R.id.WorkoutsButton)
         button.setOnClickListener {
             val intent = Intent(this@MainActivity, WorkOutPage::class.java)
             startActivity(intent)
         }
 
-        FastButton.setOnClickListener { val intent = Intent(this, FastingView::class.java).apply { putExtra(
-            AlarmClock.EXTRA_MESSAGE, "null") }
+        FastButton.setOnClickListener {
+            val intent = Intent(this, FastingView::class.java).apply {
+                putExtra(
+                    AlarmClock.EXTRA_MESSAGE, "null"
+                )
+            }
             startActivity(intent)
-            finish() }
+            finish()
+        }
+
+        val halfButton: Button = findViewById(R.id.HalfBtn)
+        val fullButton: Button = findViewById(R.id.FullBtn)
+        val text:TextView = findViewById(R.id.TotalLiters)
+
+        halfButton.setOnClickListener{
+            total += 0.5
+            text.setText(total.toString())
+        }
+        fullButton.setOnClickListener{
+            total += 1.0
+            text.setText(total.toString())
+        }
+
+
     }
 }
